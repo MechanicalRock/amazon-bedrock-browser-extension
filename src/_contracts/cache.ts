99ece2119dfea523e88a88cc81ecea9cdc46c24b
zmaleki <zee@Zees-MBP.lan>
2024-05-26 17:26:38 +0800

    refactor: refactoring caching strategy

--- src/_contracts/cache.ts
+++ src/_contracts/cache.ts
@@ -1,15 +1,20 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-import { PageMap } from './translate';
-
 /**
  * A key/value map of language pairs with text maps. The key is the
  * format of `sourceLang-targetLang` and the value is the CacheTextMap.
  */
 
 export interface CacheLangs {
-  [langPair: string]: PageMap[];
+  [langPair: string]: CachItems;
+}
+
+export interface CachItems {
+  [encodedOriginalText: string]: {
+    originalText: string;
+    translatedText: string | null;
+  };
 }
 /**
  * Maps source text (ie. "hello world!") to translated text (ie. "¡Hola mundo!").
